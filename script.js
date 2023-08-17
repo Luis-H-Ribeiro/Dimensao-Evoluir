912563d9e6199d8d5745b6a2b35fa702887bbd83
unknown <henrique_rzs@hotmail.com>
2023-08-17 20:13:54 -0300

    adicionando botões e interação com a pagina

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector('.home').classList.add('active');
+});
+
+// Função para adicionar o comportamento de clique aos botões
+function cliqueBotao(event) {
+  // Seleciona todos os botões com a classe "botao" e remove a classe "clicado"
+  var botoes = document.querySelectorAll('.botao');
+  for (var i = 0; i < botoes.length; i++) {
+    botoes[i].classList.remove('clicado');
+  }
+
+  // Adiciona a classe "clicado" ao botão que foi clicado
+  event.target.classList.add('clicado');
+}
+
+// Adiciona o evento de clique aos botões
+var botao1 = document.getElementById('botao1');
+var botao2 = document.getElementById('botao2');
+var botao3 = document.getElementById('botao3');
+var botao4 = document.getElementById('botao4');
+botao1.addEventListener('click', cliqueBotao);
+botao2.addEventListener('click', cliqueBotao);
+botao3.addEventListener('click', cliqueBotao);
+botao4.addEventListener('click', cliqueBotao);
+
+function mostrarSecao(numeroSecao) {
+  // Esconde todas as seções
+  var secoes = document.getElementsByTagName("section");
+  for (var i = 0; i < secoes.length; i++) {
+      secoes[i].style.display = "none";
+  }
+  
+  // Mostra a seção selecionada
+  document.getElementById("secao" + numeroSecao).style.display = "block";
+}
+
+ // codigo para o carrossel
 let count = 1;
 document.getElementById('radio1').checked = true;
-
+ // seta o intervalo entre as imagens
 setInterval( function(){
   nextImage();
 }, 4000)
